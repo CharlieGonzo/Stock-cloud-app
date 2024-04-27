df7fee850f32815a4eec7e514df1447f153c10a5
Charles Gonzalez Jr <mryankeecj23@gmail.com>
2024-04-27 03:35:32 -0400

    changes again

--- stocks-frontend/vite.config.js
+++ stocks-frontend/vite.config.js
@@ -4,16 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://stocks-latest.onrender.com",
-        changeOrigin: true,
-      },
-      "/stock": {
-        target: "https://stocks-latest.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
